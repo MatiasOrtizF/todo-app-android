172a940b5f6060aac204706e27d30d2619dceb62
matias <matias_ortiz_@outlook.es>
2024-02-22 17:51:29 -0300

    fix not open modal

--- app/src/main/java/com/mfo/todoapp/ui/home/TaskActivity.kt
+++ app/src/main/java/com/mfo/todoapp/ui/home/TaskActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mfo.todoapp.databinding.ActivityTaskBinding
+import com.mfo.todoapp.domain.model.Todo
 import com.mfo.todoapp.ui.home.adapter.TaskAdapter
 import com.mfo.todoapp.ui.login.MainActivity
 import com.mfo.todoapp.utils.UserData
@@ -17,7 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class TaskActivity: AppCompatActivity() {
+class TaskActivity: AppCompatActivity(){
 
     private lateinit var binding: ActivityTaskBinding
     private val taskViewModel: TaskViewModel by viewModels()
--- app/src/main/java/com/mfo/todoapp/ui/home/adapter/TaskAdapter.kt
+++ app/src/main/java/com/mfo/todoapp/ui/home/adapter/TaskAdapter.kt
@@ -1,12 +1,17 @@
 package com.mfo.todoapp.ui.home.adapter
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mfo.todoapp.R
 import com.mfo.todoapp.domain.model.Todo
 
 class TaskAdapter(val todos:List<Todo>): RecyclerView.Adapter<TaskViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
         return TaskViewHolder(layoutInflater.inflate(R.layout.todo_list, parent, false))
@@ -19,5 +24,14 @@ class TaskAdapter(val todos:List<Todo>): RecyclerView.Adapter<TaskViewHolder>() 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val item: Todo = todos[position]
         holder.bind(item)
+        val context = holder.itemView.context
+
+       // Configuración del listener de clics en el botón compartido
+        holder.itemView.findViewById<View>(R.id.sharedBtn).setOnClickListener {
+            val dialog = BottomSheetDialog(context)
+            val view = LayoutInflater.from(context).inflate(R.layout.modal, null)
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
